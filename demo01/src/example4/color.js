0c2d5edefb5b48d057dac227db9ce8a4b147aff7
guor <guor0707@qq.com>
2019-11-07 17:31:16 +0800

    使用 useReducer 和 useContext 代替 redux

--- demo01/src/example4/color.js
+++ demo01/src/example4/color.js
@@ -1,10 +1,27 @@
-import React, { createContext } from 'react';
+import React, { createContext, useReducer } from 'react';
 // 用 createContext 创建一个容器（组件）
 const ColorContext = createContext({})
+
+// 更改颜色方法的常量名
+export const UPDATE_COLOR = "UPDATE_COLOR"
+
+const reducer= (state,action)=>{
+    switch(action.type){
+        case UPDATE_COLOR:
+            return action.color
+        default:
+            return state
+    }
+}
+
+
 // color 可以看作一个容器，用它包裹的组件可以接收到它的 value 值
 const Color = (props) => {
+  // 把color、dispatch共享出去
+  const [color, dispatch] = useReducer(reducer, 'blue')
+
   return (
-    <ColorContext.Provider value={{color: 'blue'}}>
+    <ColorContext.Provider value={{color, dispatch}}>
       { props.children }
     </ColorContext.Provider>
   )
